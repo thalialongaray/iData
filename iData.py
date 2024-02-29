ee7a5567c43617eeb4891b0cbbdc08fc1d2a8221
thalialongaray <56452075+thalialongaray@users.noreply.github.com>
2024-02-28 23:09:44 -0300

    Update iData.py

--- iData.py
+++ iData.py
@@ -1,23 +1,12 @@
 from pymongo import MongoClient
-import json, time, random
+import json
 
-def interagir():
-    arq = open('interacao.json', "r", encoding='utf-8')
-    docs = json.loads(arq.read())
-    arq.close()
-
-    timeout = time.time()
-    for i in docs:
-        timeout += random.randint(0, 15)
-        while True:
-            if timeout < time.time():
-                db['interacao'].insert_one(i)
-                print(f'{i} --- {time.ctime(timeout)}')
-                break
-
-def limparColecoes(colecoes):
-    for i in colecoes:
-        db.drop_collection(i)
+def testarConexao():
+    try:
+        db.command('ping')
+        print("You're connected to iData on MongoDB!")
+    except Exception as e:
+        print(e)
 
 def popularColecoes(colecoes):
     for i in colecoes:
@@ -26,22 +15,14 @@ def popularColecoes(colecoes):
         db[i].insert_many(docs)
         arq.close()
 
-def buscarAmizadeMaisRelevante():
-    docs = db["amizade_pagerank"].find()
-    doc = None
-    relevancia = 0
-    for i in docs:
-        maisRelevante = db["amizade_pagerank"].find_one({"rank_Adjacencia.0.1": {"$gt": relevancia}}, {"rank_Adjacencia": 1})
-        if maisRelevante:
-            for i in maisRelevante['rank_Adjacencia']:
-                if i[1] > relevancia:
-                    relevancia = i[1]
-                    doc = maisRelevante
-        else:
-            break
-    print(f'{relevancia} - {doc}')
+def limparColecoes(colecoes):
+    for i in colecoes:
+        db.drop_collection(i)
 
-cliente = MongoClient("mongodb://localhost:27017/")
-db = cliente["iData"]
+def desconectar():
+    client.close()
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["iData"]
 
 colecoes = ["objeto", "interacao", "ambiente_ona", "amizade_pagerank", "classe"]
