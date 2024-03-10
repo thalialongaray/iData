b0156dfa12e812fd0b810a365d65408ab4bb678a
thalialongaray <56452075+thalialongaray@users.noreply.github.com>
2024-03-10 19:59:22 -0300

    Update CRUD_teste.py with function conectar call

--- CRUD_teste.py
+++ CRUD_teste.py
@@ -1,27 +1,29 @@
-from CRUD import *
+from iData import *
+
+client, db = conectar()
 
 def createTeste():
     f = open('docs_teste.json', 'r', encoding='utf-8')
     docs = json.loads(f.read())
-    print(create('objeto', docs['objeto'], True))
-    print(create('interacao', docs['interacao'], True))
+    print(create(db, 'objeto', docs['objeto'], True))
+    print(create(db, 'interacao', docs['interacao'], True))
     f.close()
 
 def createTeste2(colecao):
-    print(create(colecao, read(colecao, filtroSaida = {'_id': 0}, many = True), True))
+    print(create(db, colecao, read(colecao, filtroSaida = {'_id': 0}, many = True), True))
     
 def readTeste():
-    print(read('objeto', {'obj_Id': {'$gt': 5, '$lt': 50}}, many = True))
+    print(read(db, 'objeto', {'obj_Id': {'$gt': 5, '$lt': 50}}, many = True))
 
 def updateTeste():
-    id = read('objeto')['obj_Id']
-    print(update('objeto', {'$set': {'obj_Id': id + 20}}).matched_count)
-    print(id, read('objeto', {'obj_Id': id + 20}))
+    id = read(db, 'objeto')['obj_Id']
+    print(update(db, 'objeto', {'$set': {'obj_Id': id + 20}}).matched_count)
+    print(id, read(db, 'objeto', {'obj_Id': id + 20}))
 
 def deleteTeste():
-    print(delete('objeto', {'obj_Id': {'$gt': 10}}, True).deleted_count)
+    print(delete(db, 'objeto', {'obj_Id': {'$gt': 10}}, True).deleted_count)
 
 def replicarDocumentos():
     for i in range(0, 3):
         [createTeste2(colecao) for colecao in colecoes]
-        [read(colecao, many = True) for colecao in colecoes]
+        [read(db, colecao, many = True) for colecao in colecoes]
