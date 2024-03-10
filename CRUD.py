82c33ff62b413a59473a6ab1463696b59eaf58e4
thalialongaray <56452075+thalialongaray@users.noreply.github.com>
2024-03-10 19:54:24 -0300

    Update CRUD.py with function readLimit

--- CRUD.py
+++ CRUD.py
@@ -1,21 +1,22 @@
-from iData import *
-
-def create(colecao, docs, many = False):
+def create(db, colecao, docs, many = False):
     if many:
         return db[colecao].insert_many(docs)
     return db[colecao].insert_one(docs)
 
-def read(colecao, filtroConsulta = {}, filtroSaida = {}, many = False):
+def read(db, colecao, filtroConsulta = {}, filtroSaida = {}, many = False):
     if many:
         return [document for document in db[colecao].find(filtroConsulta, filtroSaida)]
     return db[colecao].find_one(filtroConsulta, filtroSaida)
 
-def update(colecao, atualizacao, filtro = {}, many = False):
+def readLimit(db, colecao, limit, filtroConsulta = {}, filtroSaida = {}):
+    return [document for document in db[colecao].find(filtroConsulta, filtroSaida).limit(limit)]
+
+def update(db, colecao, atualizacao, filtro = {}, many = False):
     if many:
         return db[colecao].update_many(filtro, atualizacao)
     return db[colecao].update_one(filtro, atualizacao)
 
-def delete(colecao, filtro = {}, many = False):
+def delete(db, colecao, filtro = {}, many = False):
     if many:
         return db[colecao].delete_many(filtro)
     return db[colecao].delete_one(filtro)
