15b620ac1623da0f94f9717d719ea31c29a64e42
thalialongaray <56452075+thalialongaray@users.noreply.github.com>
2024-03-10 19:51:34 -0300

    Add performance test files

---
+++ iDataBuscaAmizade.py
@@ -0,0 +1,49 @@
+from iData import *
+import json, time
+
+def buscarAmizadeMaisRelevante():
+    count = db["amizade_pagerank"].count_documents({})
+    
+    idObj = None
+    idObjAdj = None
+    relevancia = 0
+    num = 0
+
+    for i in range(0, count):
+        maisRelevante = read(db, "amizade_pagerank", {"rank_Adjacencia.0.1": {"$gt": relevancia}})
+        
+        if maisRelevante:
+            idObj = maisRelevante['rank_Objeto']
+            idObjAdj = maisRelevante['rank_Adjacencia'][0][0]
+            relevancia = maisRelevante['rank_Adjacencia'][0][1]
+            num += 1
+        else:
+            break
+
+    return idObj, idObjAdj, relevancia, num
+
+vColecoes = ["objeto", "amizade_pagerank"]
+numeroDeDocs = [10, 50, 100, 1000]
+
+for i in numeroDeDocs:
+    arqObjeto = open(f'{i}objetos.json', "r", encoding='utf-8')
+    arqAmizade = open(f'{i}amizades.json', "r", encoding='utf-8')
+    docs = {"objeto": [], "amizade_pagerank": []}
+    docs["objeto"] = json.loads(arqObjeto.read())
+    docs["amizade_pagerank"] = json.loads(arqAmizade.read()) 
+    arqObjeto.close()
+    arqAmizade.close()
+
+    client, db = conectar()
+    [delete(db, colecao, many = True) for colecao in vColecoes]
+    [create(db, colecao, docs[colecao], many = True) for colecao in vColecoes]
+    desconectar(client)
+    time.sleep(300)
+
+    client, db = conectar()
+    idObj, idObjAdj, relevancia, num = buscarAmizadeMaisRelevante()
+    obj = read(db, "objeto", {"obj_Id": idObj})
+    objAdj = read(db, "objeto", {"obj_Id": idObjAdj})
+    print(f'Número de documentos: {i}\nMaior relevancia encontrada: {relevancia}\nNumero de consultas necessarias: {num}\nAmizade mais relevante:\n\tObjeto:\n\t\t{obj}\n\tObjeto Adjacente:\n\t\t{objAdj}\n')
+    desconectar(client)
+    time.sleep(300)
---
+++ iDataPerformance.py
@@ -0,0 +1,20 @@
+from iData import *
+import json, time
+
+arq1k = open(f'docs1k.json', "r", encoding='utf-8')
+docs1k = json.loads(arq1k.read())
+arq1k.close()
+
+numeroDeDocs = [10, 50, 100, 1000]
+for i in numeroDeDocs:
+    client, db = conectar()
+    testarConexao(client)
+    
+    [delete(db, colecao, many = True) for colecao in colecoes]
+    time.sleep(30)
+    [create(db, colecao, docs1k[colecao][:i], many = True) for colecao in colecoes]
+    time.sleep(30)
+    [read(db, colecao, many = True) for colecao in colecoes]
+    
+    desconectar(client)
+    time.sleep(300)
